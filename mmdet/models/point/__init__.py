5376f0b3533901ee54c227bc2ee04d5e08e7b15d
filiperobotic <filipe.rolim@gmail.com>
2025-02-05 16:59:53 -0300

    core

--- mmdet/models/point/__init__.py
+++ mmdet/models/point/__init__.py
@@ -1,12 +1,4 @@
-# from .locator import BasicLocator
-# from .P2BNet import P2BNet
-# from .NoiseBox import NoiseBox
-# from .EP2BNet import EP2BNet
-from .detectors.ENoiseBox import ENoiseBox
-from .detectors.ENoiseBox_retrain import ENoiseBoxRetrain
-
-# from .P2Seg import P2Seg
-
-__all__ = ['ENoiseBox', 'ENoiseBoxRetrain'
-           # 'BasicLocator', 'P2BNet', 'NoiseBox', 'EP2BNet',  'P2Seg'
-           ]
+from .dense_heads import *
+from .detectors import *
+from .roi_heads import *
+from .losses import *
