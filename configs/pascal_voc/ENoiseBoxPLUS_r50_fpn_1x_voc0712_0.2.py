e516a720c6a88a0be67d87fc30251d770ba9bf29
filiperobotic <filipe.rolim@gmail.com>
2025-02-05 16:41:07 -0300

    path

--- configs/pascal_voc/ENoiseBoxPLUS_r50_fpn_1x_voc0712_0.2.py
+++ configs/pascal_voc/ENoiseBoxPLUS_r50_fpn_1x_voc0712_0.2.py
@@ -4,6 +4,12 @@ _base_ = [
     # '../../../configs/_base_/schedules/schedule_1x.py',
     '../_base_/default_runtime.py'
 ]
+
+custom_imports = dict(
+    imports=['mmdet.models.detectors.ENoiseBox'],  # Caminho correto para o módulo
+    allow_failed_imports=False
+)
+
 norm_cfg = dict(type='GN', num_groups=32, requires_grad=True)  # add
 debug = False
 # model settings
