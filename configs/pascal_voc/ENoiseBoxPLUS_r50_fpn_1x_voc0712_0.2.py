74d718f63e05e2a1854dd73cbd80169f37d59488
filiperobotic <filipe.rolim@gmail.com>
2025-03-20 13:57:00 -0300

    debug

--- configs/pascal_voc/ENoiseBoxPLUS_r50_fpn_1x_voc0712_0.2.py
+++ configs/pascal_voc/ENoiseBoxPLUS_r50_fpn_1x_voc0712_0.2.py
@@ -254,10 +254,10 @@ test_pipeline = [
     dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
     # avoid bboxes being resized
      dict(type='LoadAnnotations', with_bbox=True),
-    # dict(
-    #     type='PackDetInputs',
-    #     meta_keys=('img_id', 'img_path', 'ori_shape', 'img_shape',
-    #                'scale_factor'))
+    dict(
+        type='PackDetInputs',
+        meta_keys=('img_id', 'img_path', 'ori_shape', 'img_shape',
+                   'scale_factor'))
 ]
 
 
