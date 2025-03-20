66bd1eae6b754c2ac017420aa692270b679997c0
filiperobotic <filipe.rolim@gmail.com>
2025-03-20 14:39:56 -0300

    debug

--- mmdet/apis/test.py
+++ mmdet/apis/test.py
@@ -35,11 +35,11 @@ def single_gpu_test(model,
     counter = 0
     for i, data in enumerate(data_loader):
         with torch.no_grad():
-            print(f"counter = {counter}/{i}")
-            counter+=1
-            if i>246:
+            # print(f"counter = {counter}/{i}")
+            # counter+=1
+            # if i>246:
             
-                import pdb;pdb.set_trace()
+            #     import pdb;pdb.set_trace()
 
             result = model(return_loss=False, rescale=True, **data)
 
